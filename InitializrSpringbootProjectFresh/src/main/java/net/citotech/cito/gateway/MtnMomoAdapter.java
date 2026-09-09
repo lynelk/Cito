609e36f979ecf7ec3239383f896fda1634ad4d9b
lynelk <86437305+lynelk@users.noreply.github.com>
2026-09-09 18:26:14 +0300

    Capture exact MTN merchant callback ownership

--- InitializrSpringbootProjectFresh/src/main/java/net/citotech/cito/gateway/MtnMomoAdapter.java
+++ InitializrSpringbootProjectFresh/src/main/java/net/citotech/cito/gateway/MtnMomoAdapter.java
@@ -7,20 +7,35 @@ import org.springframework.stereotype.Component;
 public class MtnMomoAdapter extends LegacyGatewayAdapter {
     public static final String CHANNEL_CODE = "mtn_momo";
     private final ProviderEndpointExecutionService executionService;
+    private final MtnMomoCorrelationService correlationService;
 
-    public MtnMomoAdapter(ProviderEndpointExecutionService executionService) {
-        super(CHANNEL_CODE, "MTN MoMo", "UG", "UGX", LegacyGatewayIds.MTN_MOMO, "25677", "25678", "25676");
+    public MtnMomoAdapter(
+            ProviderEndpointExecutionService executionService,
+            MtnMomoCorrelationService correlationService) {
+        super(
+                CHANNEL_CODE,
+                "MTN MoMo",
+                "UG",
+                "UGX",
+                LegacyGatewayIds.MTN_MOMO,
+                "25677",
+                "25678",
+                "25676");
         this.executionService = executionService;
+        this.correlationService = correlationService;
     }
 
     @Override
     public GateWayResponse collect(PaymentGatewayRequest request) {
-        return executionService.execute(CHANNEL_CODE, "MTN MoMo", "COLLECT", request);
+        GateWayResponse response = executionService.execute(CHANNEL_CODE, "MTN MoMo", "COLLECT", request);
+        correlationService.capture(request, "COLLECT", response);
+        return response;
     }
 
     @Override
     public GateWayResponse payout(PaymentGatewayRequest request) {
-        return executionService.execute(CHANNEL_CODE, "MTN MoMo", "PAYOUT", request);
+        GateWayResponse response = executionService.execute(CHANNEL_CODE, "MTN MoMo", "PAYOUT", request);
+        correlationService.capture(request, "PAYOUT", response);
+        return response;
     }
 }
-
