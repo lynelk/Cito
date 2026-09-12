8d0a4ee45f2f13e96f98e71ba88233bbf7fddd9a
lynelk <86437305+lynelk@users.noreply.github.com>
2026-09-12 18:43:44 +0300

    Fix MTN origin migration settings column

--- InitializrSpringbootProjectFresh/src/main/resources/db/migration/V130__normalize_mtn_provider_origins.sql
+++ InitializrSpringbootProjectFresh/src/main/resources/db/migration/V130__normalize_mtn_provider_origins.sql
@@ -4,10 +4,10 @@
 
 UPDATE settings
 SET setting_value = 'https://proxy.momoapi.mtn.com'
-WHERE name = 'gw_mtn_api_url'
+WHERE setting_name = 'gw_mtn_api_url'
   AND COALESCE(setting_value, '') <> 'https://proxy.momoapi.mtn.com';
 
 UPDATE settings
 SET setting_value = 'https://sandbox.momodeveloper.mtn.com'
-WHERE name = 'gw_mtn_api_url_sandbox'
+WHERE setting_name = 'gw_mtn_api_url_sandbox'
   AND COALESCE(setting_value, '') <> 'https://sandbox.momodeveloper.mtn.com';
